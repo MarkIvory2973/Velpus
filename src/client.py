63941b6c449f7b1cb989077acabae8eae95ec4eb
MarkIvory2973 <markivory2973@gmail.com>
2023-11-18 22:03:48 +0800

    Commit

--- src/client.py
+++ src/client.py
@@ -4,62 +4,68 @@ import uuid
 import const
 
 class VelpusClient:
-    def __init__(self, proxy, uuid, msg):
+    def __init__(self, proxy, uuid, msgsetting):
         self.client = socket.socket()
         self.proxy = proxy
         self.uuid = uuid
-        self.msg = msg
+        self.msgsetting = msgsetting
         
     def Auth(self):
         self.client.connect(self.proxy)
         
-        self._SendStruct("! B 16s B", const.VELPUS_AUTH, self.uuid.bytes, self.msg)
+        self._SendStruct("! B 16s B", const.CMD.VELPUS_AUTH, self.uuid.bytes, self.msgsetting)
         
-        msg = self.client.recv(1)
-        msg = struct.unpack("! B", msg)[0]
+        msg = self.client.recv(2)
+        msg = struct.unpack("! B B", msg)[1]
         
         return msg
     
     def Connect(self, server, intype, timeout):
-        if intype == const.VELPUS_IPV4:
+        if intype == const.TYPE.VELPUS_IPV4:
             host = bytes(map(int, server[0].split(".")))
         
-        self._SendStruct("! B B 4s H f", const.VELPUS_CONNECT, intype, host, server[1], timeout)
+        self._SendStruct("! B B 4s H f", const.CMD.VELPUS_CONNECT, intype, host, server[1], timeout)
         
-        msg = self.client.recv(1)
-        msg = struct.unpack("! B", msg)[0]
+        msg = self.client.recv(2)
+        msg = struct.unpack("! B B", msg)[1]
         
         return msg
     
     def Send(self, server, intype, data):
-        if intype == const.VELPUS_IPV4:
+        if intype == const.TYPE.VELPUS_IPV4:
             host = bytes(map(int, server[0].split(".")))
             
-        self._SendStruct(f"! B B 4s H Q {len(data)}s", const.VELPUS_SEND, intype, host, server[1], len(data), data)
+        self._SendStruct(f"! B B 4s H Q {len(data)}s", const.CMD.VELPUS_SEND, intype, host, server[1], len(data), data)
         
-        msg = self.client.recv(1)
-        msg = struct.unpack("! B", msg)[0]
+        msg = self.client.recv(2)
+        msg = struct.unpack("! B B", msg)[1]
         
         return msg
         
     def Recv(self, server, intype, bufsize):
-        if intype == const.VELPUS_IPV4:
+        if intype == const.TYPE.VELPUS_IPV4:
             host = bytes(map(int, server[0].split(".")))
             
-        self._SendStruct("! B B 4s H Q", const.VELPUS_RECV, intype, host, server[1], bufsize)
+        self._SendStruct("! B B 4s H Q", const.CMD.VELPUS_RECV, intype, host, server[1], bufsize)
+        
+        msg = self.client.recv(2)
+        msg = struct.unpack("! B B", msg)[1]
+        
+        if msg != const.MSG.VELPUS_SUCCEED:
+            return msg, None
         
         data = self.client.recv(bufsize)
         
-        return data
+        return msg, data
     
     def Disconnect(self, server, intype):
-        if intype == const.VELPUS_IPV4:
+        if intype == const.TYPE.VELPUS_IPV4:
             host = bytes(map(int, server[0].split(".")))
             
-        self._SendStruct("! B B 4s H", const.VELPUS_CONNECT, intype, host, server[1])
+        self._SendStruct("! B B 4s H", const.CMD.VELPUS_CONNECT, intype, host, server[1])
         
-        msg = self.client.recv(1)
-        msg = struct.unpack("! B", msg)[0]
+        msg = self.client.recv(2)
+        msg = struct.unpack("! B B", msg)[1]
         
         return msg
     
@@ -67,10 +73,11 @@ class VelpusClient:
         data = struct.pack(format, *datas)
         return self.client.send(data)
     
-vclient = VelpusClient(("127.0.0.1", 8080), uuid.UUID("44a908c6-c0fa-4b73-bf04-174cb92c3f6c"), const.VELPUS_ALLMSG)
+vclient = VelpusClient(("127.0.0.1", 8080), uuid.UUID("44a908c6-c0fa-4b73-bf04-174cb92c3f6c"), const.TYPE.VELPUS_ALLMSG)
 print(vclient.Auth())
-print(vclient.Connect(("127.0.0.1", 80), const.VELPUS_IPV4, 0.0001))
-print(vclient.Send(("127.0.0.1", 80), const.VELPUS_IPV4, b"GET"))
-print(vclient.Recv(("127.0.0.1", 80), const.VELPUS_IPV4, 4096))
-print(vclient.Send(("127.0.0.1", 80), const.VELPUS_IPV4, b"CLOSE"))
+print(vclient.Connect(("127.0.0.1", 80), const.TYPE.VELPUS_IPV4, 0.1))
+print(vclient.Send(("127.0.0.1", 80), const.TYPE.VELPUS_IPV4, b"GET"))
+print(vclient.Recv(("127.0.0.1", 80), const.TYPE.VELPUS_IPV4, 4096))
+__import__("os").system("pause")
+print(vclient.Send(("127.0.0.1", 80), const.TYPE.VELPUS_IPV4, b"CLOSE"))
 vclient.client.close()
--- src/proxy.py
+++ src/proxy.py
@@ -3,101 +3,155 @@ import struct
 import uuid
 import const
 
-Users = {
+class VelpusProxy:
+    def __init__(self, proxy, users):
+        self.proxy = proxy
+        self.users = users
+        
+    async def Start(self):
+        server = await asyncio.start_server(self.handle_client, *self.proxy)
+        async with server:
+            await server.serve_forever()
+            
+    async def handle_client(self, client_reader, client_writer):
+        # UUID
+        UUID = None
+        # Message setting
+        MSGSetting = const.TYPE.VELPUS_ALLMSG
+        # Connections
+        connections = {}
+        
+        while True:
+            # Recieve
+            data = await client_reader.read(4096)
+            # Connection closed
+            if not data:
+                break
+            # VELPUS_AUTH: |-CMD-|-UUID-|-MSG-|
+            if data[0] == const.CMD.VELPUS_AUTH:
+                try:
+                    # Unpack
+                    auth = struct.unpack("! B 16s B", data)
+                except:
+                    # Failed to unpack
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_FAILED)
+                    continue
+                
+                # Record UUID
+                UUID = uuid.UUID(bytes=auth[1])
+                # Set message setting
+                MSGSetting = auth[2]
+                
+                if not UUID in self.users:
+                    # Invalid UUID
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_INVALID_UUID)
+                    continue
+            # VELPUS_CONNECT: |-CMD-|-INTYPE-|-IP-|-PORT-|
+            elif data[0] == const.CMD.VELPUS_CONNECT and UUID:
+                # VELPUS_IPV4
+                if data[1] == const.TYPE.VELPUS_IPV4:
+                    try:
+                        # Unpack
+                        connect = struct.unpack("! B B 4s H f", data)
+                    except:
+                        # Failed to unpack
+                        await self._SendMsg(client_writer, const.MSG.VELPUS_FAILED)
+                        continue
+                    
+                    # Unpack IP (IPv4)
+                    server = ".".join(map(str, connect[2])), connect[3]
+                    
+                try:
+                    # Connect to server
+                    connections[server] = await asyncio.wait_for(asyncio.open_connection(*server), connect[4])
+                except asyncio.TimeoutError:
+                    # Timeout
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_CONNECTION_TIMEOUT)
+                    continue
+                except:
+                    # Failed to connect
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_FAILED)
+                    continue
+            # VELPUS_SEND: |-CMD-|-INTYPE-|-IP-|-PORT-|-DATASIZE-|
+            elif data[0] == const.CMD.VELPUS_SEND and UUID:
+                # VELPUS_IPV4
+                if data[1] == const.TYPE.VELPUS_IPV4:
+                    try:
+                        # Unpack
+                        send = struct.unpack("! B B 4s H Q", data[:16])
+                    except:
+                        # Failed to unpack
+                        await self._SendMsg(client_writer, const.MSG.VELPUS_FAILED)
+                        continue
+                        
+                    # Unpack IP
+                    server = ".".join(map(str, send[2])), send[3]
+                    
+                if not server in connections:
+                    # Failed to send to a connection that has not been connected yet
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_UNCONNECTED)
+                    continue
+                    
+                # Send
+                connections[server][1].write(data[16:16 + send[4]])
+                await connections[server][1].drain()
+            # VELPUS_RECV: |-CMD-|-INTYPE-|-IP-|-PORT-|-BUFSIZE-|
+            elif data[0] == const.CMD.VELPUS_RECV and UUID:
+                # VELPUS_IPV4
+                if data[1] == const.TYPE.VELPUS_IPV4:
+                    try:
+                        # Unpack
+                        recv = struct.unpack("! B B 4s H Q", data)
+                    except:
+                        # Failed to unpack
+                        await self._SendMsg(client_writer, const.MSG.VELPUS_FAILED)
+                        continue
+                    
+                    # Unpack IP
+                    server = ".".join(map(str, recv[2])), recv[3]
+                    
+                if not server in connections:
+                    # Failed to send to a connection that has not been connected yet
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_UNCONNECTED)
+                    continue
+                    
+                # Succeed
+                if MSGSetting == const.TYPE.VELPUS_ALLMSG:
+                    await self._SendMsg(client_writer, const.MSG.VELPUS_SUCCEED)
+                    
+                # Recieve
+                data = await connections[server][0].read(recv[4])
+                # Send
+                client_writer.write(data)
+                await client_writer.drain()
+                
+                continue
+            # VELPUS_DISCONNECT: |-CMD-|-INTYPE-|-IP-|-PORT-|
+            elif data[0] == const.CMD.VELPUS_DISCONNECT:
+                # VELPUS_IPV4
+                if data[1] == const.TYPE.VELPUS_IPV4:
+                    disconnect = struct.unpack("! B B 4s H", data)
+                    server = ".".join(map(str, disconnect[2])), disconnect[3]
+                    
+                connections[server][1].close()
+                connections.pop(server)
+            else:
+                # Unknown command
+                await self._SendMsg(client_writer, const.MSG.VELPUS_UNKNOWN_CMD)
+                continue
+                
+            # Succeed
+            if MSGSetting == const.TYPE.VELPUS_ALLMSG:
+                await self._SendMsg(client_writer, const.MSG.VELPUS_SUCCEED)
+        
+    async def _SendMsg(self, client_writer, msg):
+        msg = struct.pack("! B B", const.CMD.VELPUS_MSG, msg)
+        client_writer.write(msg)
+        await client_writer.drain()
+
+users = {
     uuid.UUID("44a908c6-c0fa-4b73-bf04-174cb92c3f6c"): "2uA3F39njf$"
 }
 
-async def SendMsg(client_writer, msg):
-    msg = struct.pack("! B", msg)
-    client_writer.write(msg)
-    await client_writer.drain()
-
-async def handle_client(client_reader, client_writer):
-    UUID = None
-    MSG = None
-    servers = {}
-    
-    while True:
-        data = await client_reader.read(4096)
-        if not data:
-            break
-        # VELPUS_AUTH: |-CMD-|-UUID-|-MSG-|
-        if data[0] == const.VELPUS_AUTH:
-            try:
-                auth = struct.unpack("! B 16s B", data)
-            except:
-                await SendMsg(client_writer, const.VELPUS_FAILED)
-                break
-            
-            UUID = uuid.UUID(bytes=auth[1])
-            MSG = auth[2]
-            
-            if not UUID in Users:
-                await SendMsg(client_writer, const.VELPUS_INVALID_UUID)
-                break
-            
-            if MSG == const.VELPUS_ALLMSG:
-                await SendMsg(client_writer, const.VELPUS_SUCCEED)
-        # VELPUS_CONNECT: |-CMD-|-INTYPE-|-IP-|-PORT-|
-        elif data[0] == const.VELPUS_CONNECT and UUID:
-            # VELPUS_IPV4
-            if data[1] == const.VELPUS_IPV4:
-                try:
-                    connect = struct.unpack("! B B 4s H f", data)
-                except:
-                    await SendMsg(client_writer, const.VELPUS_FAILED)
-                
-                server = ".".join(map(str, connect[2])), connect[3]
-                
-            try:
-                servers[server] = await asyncio.wait_for(asyncio.open_connection(*server), connect[4])
-            except asyncio.TimeoutError:
-                await SendMsg(client_writer, const.VELPUS_CONNECTION_TIMEOUT)
-                continue
-            except:
-                await SendMsg(client_writer, const.VELPUS_FAILED)
-                continue
-            
-            if MSG == const.VELPUS_SUCCEED:
-                await SendMsg(client_writer, const.VELPUS_SUCCEED)
-        # VELPUS_SEND: |-CMD-|-INTYPE-|-IP-|-PORT-|-DATASIZE-|
-        elif data[0] == const.VELPUS_SEND and UUID:
-            # VELPUS_IPV4
-            if data[1] == const.VELPUS_IPV4:
-                send = struct.unpack("! B B 4s H Q", data[:16])
-                server = ".".join(map(str, send[2])), send[3]
-                
-            servers[server][1].write(data[16:16 + send[4]])
-            await servers[server][1].drain()
-            
-            await SendMsg(client_writer, const.VELPUS_SUCCEED)
-        # VELPUS_RECV: |-CMD-|-INTYPE-|-IP-|-PORT-|-BUFSIZE-|
-        elif data[0] == const.VELPUS_RECV and UUID:
-            # VELPUS_IPV4
-            if data[1] == const.VELPUS_IPV4:
-                recv = struct.unpack("! B B 4s H Q", data)
-                server = ".".join(map(str, recv[2])), recv[3]
-                
-            data = await servers[server][0].read(recv[4])
-            client_writer.write(data)
-            await client_writer.drain()
-        # VELPUS_DISCONNECT: |-CMD-|-INTYPE-|-IP-|-PORT-|
-        elif data[0] == const.VELPUS_DISCONNECT:
-            # VELPUS_IPV4
-            if data[1] == const.VELPUS_IPV4:
-                disconnect = struct.unpack("! B B 4s H", data)
-                server = ".".join(map(str, disconnect[2])), disconnect[3]
-                
-            servers[server][1].close()
-            servers.pop(server)
-            
-            await SendMsg(client_writer, const.VELPUS_SUCCEED)
-            
-    client_writer.close()
-
-async def main():
-    server = await asyncio.start_server(handle_client, "127.0.0.1", 8080)
-    async with server:
-        await server.serve_forever()
-
-asyncio.run(main())
+vproxy = VelpusProxy(("127.0.0.1", 8080), users)
+asyncio.run(vproxy.Start())
